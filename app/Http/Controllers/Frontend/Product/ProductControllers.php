7056883a49899e0bb6de862e356430f61e75e938
viantonugroho <viantonugroho@student.gunadarma.ac.id>
2022-02-09 10:04:23 +0700

    add views be and fe

---
+++ app/Http/Controllers/Frontend/Product/ProductControllers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductControllers extends Controller
+{
+    //
+}
