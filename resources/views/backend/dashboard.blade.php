99b9a27e4a06532671dbbdf49a9c2b2fe7a1cf48
viantonugroho <viantonugroho@student.gunadarma.ac.id>
2022-02-11 16:02:51 +0700

    fixing dashboard and auth admin

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -88,7 +88,7 @@
         </div>
         <!-- /.row -->
         <!-- Main row -->
-        <div class="row">
+        {{-- <div class="row">
           <!-- Left col -->
           <section class="col-lg-7 connectedSortable">
             <!-- Custom tabs (Charts with tabs)-->
@@ -598,7 +598,7 @@
             <!-- /.card -->
           </section>
           <!-- right col -->
-        </div>
+        </div> --}}
         <!-- /.row (main row) -->
       </div><!-- /.container-fluid -->
     </section>
