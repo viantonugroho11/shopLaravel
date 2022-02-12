44e48f14b4f2369433efb728459fe2910cf847f8
viantonugroho <viantonugroho@student.gunadarma.ac.id>
2022-02-12 15:29:01 +0700

    fixing route auth

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+Route::get('/admin', [App\Http\Controllers\Backend\Auth\AuthControllers::class, 'getLogin'])->name('adminlogin');
+Route::post('/admin', [App\Http\Controllers\Backend\Auth\AuthControllers::class, 'postLogin']);
+Route::get('/admin/logout', [App\Http\Controllers\Backend\Auth\AuthControllers::class, 'postLogout'])->name('adminlogout');
+
 // Route::get('/')
